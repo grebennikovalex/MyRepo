8313b26138f383e9e141eb1e27c389eefefdee3b
grebennikovalex <a-greb@yandex.ru>
2019-12-06 00:23:42 +0300

    New slider
    A new slider added to change blue channel

--- clip_script.js
+++ clip_script.js
@@ -5,10 +5,11 @@
 	let flash;
 	let timer = 200;
 	let scale = 1.43;
+	let rangeb = 255;
 	let flag = true;
 	let g;
 	let r;
-	//let b;
+	let b;
 	let s = 0;
 	const reloadBtn = document.getElementById('reload');
 	const flashingBtn = document.getElementById('flashing');
@@ -23,7 +24,7 @@
 	
 	let rangeSlider1 = document.getElementById('theRange1');
 	let rangeSlider2 = document.getElementById('theRange2');
-	
+	let rangeSlider3 = document.getElementById('theRange3');
 	
 	let flashing = () => flash = setInterval(colors, 25);
 			
@@ -31,10 +32,10 @@
 		
 			if (flag) {flashing(); flag = false;} else return;
 			
-	};		
-		
-	
+		};		
+			
 	let reload = () => window.location.reload(false);
+	
 	let stopFlashing = () => {
 		
 			clearInterval(flash);
@@ -49,23 +50,23 @@
 	
 	rangeSlider1.oninput = () => timer = rangeSlider1.value;
 	rangeSlider2.oninput = () => scale = parseInt(rangeSlider2.value)/100;
-		
+	rangeSlider3.oninput = () => rangeb = parseInt(rangeSlider3.value);	
 		
 		
     let colors = () => {
 		
 	
-		let b = 255; 
-		let r = 0;
+		b = rangeb;
+		
 		s = s + Math.PI*2/parseInt(timer);
 		
-		g = 128 + ( 128 * Math.sin(s) )* -1;
-		//r = 128 + ( 128 * Math.sin(s) )* -1;
+		g = 128 + ( 128 * Math.sin(s) );
+		r = 128 + ( 128 * Math.sin(s) )* -1;
 		//b = 128 + ( 128 * Math.sin(s) );
 						
 				for ( let i = 0; i < 18; i++ ) {
 						  
-				changeColor(r, 255-g, b, i);
+				changeColor(255-r, g, b, i);
 
 				colorNumber[i].innerHTML = ` R `+ r.toFixed(2) + ` G ` + g.toFixed(2) + ` B	` + b.toFixed(2) + `   ` + timer;	
 
@@ -73,9 +74,9 @@
 					rangeBar.style.background = 'RGB(' + r + ',' + g + ',' + b + ')';
 					});
 								
-				b -=15;
+				b = b - rangeb/18;
 				
-				r +=15;
+				//r +=15;
 				
 				
 												
