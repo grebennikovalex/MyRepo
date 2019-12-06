ca2df2aa4dd100ff6f44be31912097f8d67f6a53
grebennikovalex <a-greb@yandex.ru>
2019-12-07 02:33:16 +0300

    Ehhh...

--- clip_script.js
+++ clip_script.js
@@ -11,38 +11,24 @@
 	let r;
 	let b;
 	let s = 0;
+	
+	
+				
 	const reloadBtn = document.getElementById('reload');
 	const flashingBtn = document.getElementById('flashing');
 	const stopFlashingBtn = document.getElementById('stopFlasing');
 	const colorNumber = document.getElementsByClassName('col');
 	const rangeBars = document.getElementsByClassName('range');
 	const cells = document.getElementsByClassName('cube');
-<<<<<<< HEAD
 	
-<<<<<<< HEAD
-	[...cells].forEach(cell => {							
-							
-							cell.newScale = { 
-										clickCount: 0.1			
-									};
-		});
+	Element.prototype.clickCounter = {
+			  counter: 0.1,
+			  			};
 		
 	
 	let changeColor = (r, g, b, i) => cells[i].style.background = 'RGB(' + r + ',' + g + ',' + b + ')';
 		
 	
-=======
-	
-=======
->>>>>>> parent of 3b1505d... Trying to work with objects
-	let changeColor = (r, g, b, i) => {
-		cells[i].style.background = 'RGB(' + r + ',' + g + ',' + b + ')';
-		cells[i].style.transform = 'scale(' + scale + ', ' + scale + ')';
-	};
-<<<<<<< HEAD
->>>>>>> master
-=======
->>>>>>> parent of 3b1505d... Trying to work with objects
 	
 	let rangeSlider1 = document.getElementById('theRange1');
 	let rangeSlider2 = document.getElementById('theRange2');
@@ -66,39 +52,15 @@
 		
 		};
 		
-<<<<<<< HEAD
-<<<<<<< HEAD
 		
-=======
-	//let cellScale = (cell) => cell.style.transform = 'scale(0.2, 0.2)';
-		
-	
->>>>>>> master
-=======
-	//let cellScale = (cell) => cell.style.transform = 'scale(0.2, 0.2)';
-		
-	
->>>>>>> parent of 3b1505d... Trying to work with objects
 	reloadBtn.addEventListener('click', reload);
 	flashingBtn.addEventListener('click', startFlash);	
 	stopFlashingBtn.addEventListener('click', stopFlashing);
 
-<<<<<<< HEAD
-<<<<<<< HEAD
 	
 	
 	
 	
-=======
-	/* [...cells].forEach(cell => {
-					cell.addEventListener('click', cellScale(this.cell));
-					}); */
->>>>>>> master
-=======
-	/* [...cells].forEach(cell => {
-					cell.addEventListener('click', cellScale(this.cell));
-					}); */
->>>>>>> parent of 3b1505d... Trying to work with objects
 	
 	rangeSlider1.oninput = () => timer = rangeSlider1.value;
 	rangeSlider2.oninput = () => scale = parseInt(rangeSlider2.value)*0.0043 + 1;
@@ -111,34 +73,18 @@
 			document.getElementById('sliderValue2').innerHTML = rangeSlider2.value;
 	
 			[...cells].forEach(cell => {
-<<<<<<< HEAD
-<<<<<<< HEAD
 				
-					cell.onmousedown = () => cell.newScale.clickCount +=0.1;
-					cell.style.transform = 'scale(' + scale + ', ' + scale + ')';
+					cell.onmousedown = () => cell.counter +=0.1;
+					cell.style.transform = 'scale(' + (scale - cell.counter) + ', ' + scale + ')';
 					
 					
-=======
-					cell.style.transform = 'scale(' + scale + ', ' + scale + ')';
->>>>>>> master
-=======
-					cell.style.transform = 'scale(' + scale + ', ' + scale + ')';
->>>>>>> parent of 3b1505d... Trying to work with objects
 					});
 			};
 			
 	let sliderNumber3 =() => document.getElementById('sliderValue3').innerHTML = rangeb;		
 			
 			
-<<<<<<< HEAD
-<<<<<<< HEAD
 				
-=======
-					
->>>>>>> master
-=======
-					
->>>>>>> parent of 3b1505d... Trying to work with objects
 					
 		
     let colors = () => {
